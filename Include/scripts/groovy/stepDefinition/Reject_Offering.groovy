f1a7f2a0a212bdeea4461a7cf6cf834d83766974
Tommy Chaniago <tommy.chaniago42@gmail.com>
2023-02-07 17:43:24 +0000

    Replace Reject_Offering.groovy

--- Include/scripts/groovy/stepDefinition/Reject_Offering.groovy
+++ Include/scripts/groovy/stepDefinition/Reject_Offering.groovy
@@ -48,66 +48,81 @@ class Reject_Offering {
 	@Given("Seller has login into the secondhand store website")
 	public void seller_has_login_into_the_secondhand_store_website() {
 		WebUI.openBrowser('secondhand-store.herokuapp.com/login')
+		WebUI.maximizeWindow()
 	}
-	
+
 	@When("Seller input email {string}")
 	public void seller_input_email(String Email) {
 		WebUI.sendKeys(findTestObject('Page_Login/login_input_email'), Email)
 	}
-	
+
 	@When("Seller input Password {string}")
 	public void seller_input_Password(String Password) {
 		WebUI.sendKeys(findTestObject('Page_Login/login_input_password'), Password)
 	}
-	
+
 	@When("Seller click LOGIN Button")
 	public void seller_click_LOGIN_Button() {
 		WebUI.click(findTestObject('Page_Login/login_btn'))
 	}
-	
+
 	@When("Seller click List Menu")
 	public void seller_click_List_Menu() {
 		WebUI.callTestCase(findTestCase('Pages/Reject Offering/Click List Menu'), [:], FailureHandling.STOP_ON_FAILURE)
 	}
-	
+
 	@When("Seller click Interested Menu")
 	public void seller_click_Interested_Menu() {
 		WebUI.callTestCase(findTestCase('Pages/Reject Offering/Click Diminati Menu'), [:], FailureHandling.STOP_ON_FAILURE)
 	}
-	
+
 	@When("Seller click Product")
 	public void seller_click_Product() {
 		WebUI.callTestCase(findTestCase('Pages/Reject Offering/Click Interested Product 1'), [:], FailureHandling.STOP_ON_FAILURE)
 	}
-	
+
 	@When("Seller click Reject Button")
 	public void seller_click_Reject_Button() {
 		WebUI.callTestCase(findTestCase('Pages/Reject Offering/Click Reject'), [:], FailureHandling.STOP_ON_FAILURE)
 	}
-	
+
 	@When("Seller click Yes Button")
 	public void seller_click_Yes_Button() {
 		WebUI.callTestCase(findTestCase('Pages/Reject Offering/Click Yes'), [:], FailureHandling.STOP_ON_FAILURE)
 	}
-	
+
 	@When("Seller wait for Message displayed {string}")
 	public void seller_wait_for_Message_displayed(String string) {
 		WebUI.waitForElementVisible(findTestObject('Page_Reject_Offering/Msg_Tawaran_produk_ditolak'), 2)
 	}
-	
+
 	@When("Seller click Productt")
 	public void seller_click_Productt() {
 		WebUI.callTestCase(findTestCase('Pages/Reject Offering/Click Interested Product 2'), [:], FailureHandling.STOP_ON_FAILURE)
 	}
-	
+
 	@When("Seller click Rejects Button")
 	public void seller_click_Rejects_Button() {
 		WebUI.callTestCase(findTestCase('Pages/Reject Offering/Click Reject'), [:], FailureHandling.STOP_ON_FAILURE)
 	}
-	
-	@Then("Seller click No Button")
+
+	@When("Seller click No Button")
 	public void seller_click_No_Button() {
 		WebUI.callTestCase(findTestCase('Pages/Reject Offering/Click No'), [:], FailureHandling.STOP_ON_FAILURE)
 	}
 	
+	@When("Seller click Back Button")
+	public void seller_click_Back_Button() {
+		WebUI.click(findTestObject('Page_Reject_Offering/arrow-left_button'))
+	}
+	
+	@When("Seller click Interestedd Menu")
+	public void seller_click_Interestedd_Menu() {
+		WebUI.callTestCase(findTestCase('Pages/Reject Offering/Click Diminati Menu'), [:], FailureHandling.STOP_ON_FAILURE)
+	}
+	
+	@Then("Product still in Interested Menu")
+	public void product_still_in_Interested_Menu() {
+		WebUI.verifyElementVisible(findTestObject('Page_Reject_Offering/p_Samsung_Galaxy_A8_Elektronik'))
+	}
 }
