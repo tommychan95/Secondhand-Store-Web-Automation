21b2e9ae1dcecc75a21d69564f253d13e2f037d6
Oza Zativa <zoriza0@gmail.com>
2023-02-13 17:15:57 +0700

    add test suite

--- Test Suites/Feature Profile.ts
+++ Test Suites/Feature Profile.ts
@@ -11,4 +11,39 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>f3e753fc-e15d-42f6-8d92-8897f1332839</testSuiteGuid>
+   <testCaseLink>
+      <guid>c56bd9e5-2ae7-4aaf-8cf7-3e00b85249e6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Profile/PF001 - User want to change name</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f175d368-4ed8-4c0f-86cc-0fe81e972edb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Profile/PF002 - User want to change city</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7312f930-b504-4b9b-9298-fb9edf85a71f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Profile/PF003 - User want to change address</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f6921542-ec40-4d4e-8077-2cb9b62c80cb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Profile/PF004 - User want to change phone number</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dd69368f-eaf6-4b9a-b5d6-634cb20e2397</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Profile/PF005 - User want to change profile picture</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
